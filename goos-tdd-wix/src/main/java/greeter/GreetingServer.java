53ac83e1572d0a57d67836b72f1762b76b36ea76
miguelarian <miguelvr88@gmail.com>
2024-07-08 16:13:21 +0200

    refactor to use httpHandler lambda expression

--- goos-tdd-wix/src/main/java/greeter/GreetingServer.java
+++ goos-tdd-wix/src/main/java/greeter/GreetingServer.java
@@ -11,15 +11,12 @@ import java.net.InetSocketAddress;
 public class GreetingServer {
     public static void main(String... args) throws IOException {
         final HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/greeting", new HttpHandler() {
-            @Override
-            public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello, World!";
-                exchange.sendResponseHeaders(200, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            }
+        server.createContext("/greeting", handler -> {
+            String response = "Hello, World!";
+            handler.sendResponseHeaders(200, response.length());
+            OutputStream os = handler.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         });
         server.start();
     }
